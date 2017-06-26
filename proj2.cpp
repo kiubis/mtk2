789ddf956f2d8ea8b626499fbe4ce5adac2a3fdb
kiubis <kiubis96@gmail.com>
2017-06-26 19:30:05 +0200

    Update proj2.cpp

--- proj2.cpp
+++ proj2.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-void wyswietl(char tab[][14]);											// funckja do wyswietlania pola
+void wyswietl(char tab[][14]);						// funckja do wyswietlania pola
 void sprawdzenie(char tab[][14], bool *wygral , char *wygrany);		// funkcja do sprawdzania wygranej
 
 int main(){
@@ -27,18 +27,18 @@ char tab[14][14]={
 //	  0   1   2   3   4   5   6   7   8   9  10  11  12  13
 
 int rundy = 1;				// zmienan do rund
-bool wygral = false;	// zmienna do sprawdzenia, czy rozgrywka juz zostla zakonczona (wygral ktos)
+bool wygral = false;			// zmienna do sprawdzenia, czy rozgrywka juz zostla zakonczona (wygral ktos)
 char wygrany = 0;			// zmienna do okreslenia kto wygral
 
 // petla gry
 do{
 	
-if(rundy%2){						// jesli nieparzysta runda 
+if(rundy%2){				// jesli nieparzysta runda 
 
-wyswietl(tab);						// pokaz pole gry
+wyswietl(tab);				// pokaz pole gry
 cout << "Runda gracza X" << endl;	// ktory gracz
-char x = 0;							// zmienna do wspolrzednej x
-int y = 0;							// zmienna do wspolrzednej y
+char x = 0;				// zmienna do wspolrzednej x
+int y = 0;				// zmienna do wspolrzednej y
 
 // przyjecie wspolrzednych 
 cout << "Podaj wspolrzedna X (np. A):";
@@ -46,45 +46,44 @@ cin  >> x;
 cout << "Podaj wspolrzedna Y (np. 1):";
 cin >> y;
 
-int wspX = 0 , wspY = 0;			// zmienne do okreslenia w ktore pole wpisujemy X lub O
+int wspX = 0 , wspY = 0;		// zmienne do okreslenia w ktore pole wpisujemy X lub O
 
-switch(x){							// w zaleznosci od podanej litery (wartosc w ascii)
-case 65:							// A = 65
-	wspX = 3;						// pola z lewej
+switch(x){				// w zaleznosci od podanej litery (wartosc w ascii)
+case 65:				// A = 65
+	wspX = 3;			// pola z lewej
 	break;
-case 66:							// B = 66
-	wspX = 7;						// srodkowe pola
+case 66:				// B = 66
+	wspX = 7;			// srodkowe pola
 	break;
-case 67:							// C = 67
-	wspX = 11;						// pola z prawej
+case 67:				// C = 67
+	wspX = 11;			// pola z prawej
 	break;
 }
 
-switch(y){							// w zaleznosci od cyfry
+switch(y){				// w zaleznosci od cyfry
 case 1:								
-	wspY = 3;						// pola od gory
+	wspY = 3;			// pola od gory
 	break;
 case 2:
-	wspY = 7;						// srodkowe pola
+	wspY = 7;			// srodkowe pola
 	break;
 case 3:
-	wspY = 11;						// pola od dolu
+	wspY = 11;			// pola od dolu
 	break;
 }
 
-tab[wspY][wspX] = 'X';				// wpisanie znaku
+tab[wspY][wspX] = 'X';			// wpisanie znaku
 
 sprawdzenie(tab, &wygral , &wygrany);	// sprawdzenie czy ktos wygral
 
-rundy++;							// zwiekszenie ilosci rund 
+rundy++;				// zwiekszenie ilosci rund 
 
-} else {							// jesli parzysta runda 
+} else {				// jesli parzysta runda 
 
-wyswietl(tab);						// pokaz pole gry
-
+wyswietl(tab);				// pokaz pole gry
 cout << "Runda gracza O" << endl;	// ktory gracz
-char x = 0;							// zmienna do wspolrzednej x
-int y = 0;							// zmienna do wspolrzednej y
+char x = 0;				// zmienna do wspolrzednej x
+int y = 0;				// zmienna do wspolrzednej y
 
 // przyjecie wspolrzednych
 cout << "Podaj wspolrzedna X (np. A):";
@@ -92,47 +91,47 @@ cin  >> x;
 cout << "Podaj wspolrzedna Y (np. 1):";
 cin >> y;
 
-int wspX = 0 , wspY = 0;			// zmienne do okreslenia w ktore pole wpisujemy X lub O
+int wspX = 0 , wspY = 0;		// zmienne do okreslenia w ktore pole wpisujemy X lub O
 
-switch(x){							// w zaleznosci od podanej litery (wartosc w ascii)
-case 65:							// A = 65
-	wspX = 3;						// pola z lewej
+switch(x){				// w zaleznosci od podanej litery (wartosc w ascii)
+case 65:				// A = 65
+	wspX = 3;			// pola z lewej
 	break;
-case 66:							// B = 66
-	wspX = 7;						// srodkowe pola
+case 66:				// B = 66
+	wspX = 7;			// srodkowe pola
 	break;
-case 67:							// C = 67
-	wspX = 11;						// pola z prawej
+case 67:				// C = 67
+	wspX = 11;			// pola z prawej
 	break;
 }
 
-switch(y){							// w zaleznosci od cyfry
+switch(y){				// w zaleznosci od cyfry
 case 1:								
-	wspY = 3;						// pola od gory
+	wspY = 3;			// pola od gory
 	break;
 case 2:
-	wspY = 7;						// srodkowe pola
+	wspY = 7;			// srodkowe pola
 	break;
 case 3:
-	wspY = 11;						// pola od dolu
+	wspY = 11;			// pola od dolu
 	break;
 }
 
-tab[wspY][wspX] = 'O';				// wpisanie znaku
+tab[wspY][wspX] = 'O';			// wpisanie znaku
 
 sprawdzenie(tab, &wygral , &wygrany);	// sprawdzenie czy ktos wygral
 
-rundy++;							// zwiekszenie ilosci rund 
+rundy++;				// zwiekszenie ilosci rund 
 }
 
-}while(!wygral && rundy < 10);	// petla gry bedzie przerwana jesli zmienna wygral bedzie miala wartosc true lub skoncza sie rundy (czyli pola zostana zapelnione) && - operator logiczny I 
+}while(!wygral && rundy < 10);		// petla gry bedzie przerwana jesli zmienna wygral bedzie miala wartosc true lub skoncza sie rundy (czyli pola zostana zapelnione) && - operator logiczny I 
 
-wyswietl(tab);						// wyswietlenie pola
+wyswietl(tab);				// wyswietlenie pola
 
-if(wygral){						// jesli ktos wygral 
+if(wygral){				// jesli ktos wygral 
 	cout << "Wygral gracz " << wygrany << "!" << endl;	// wypisz kto wygral
-} else {							// jesli nikt nie wygral
-cout << "Remis!" << endl;			// wypisz remis
+} else {				// jesli nikt nie wygral
+cout << "Remis!" << endl;		// wypisz remis
 }
 
 return 0;
@@ -148,7 +147,7 @@ void wyswietl(char tab[][14]){
 	}
 }
 
-void sprawdzenie(char tab[][14], bool *wygral , char *wygrany){						// sprawdzenie warunkow (jesli pola w linii sa sobie rowne i sa rozne od spacji to ustalany jest zwyciezca)
+void sprawdzenie(char tab[][14], bool *wygral , char *wygrany){		// sprawdzenie warunkow (jesli pola w linii sa sobie rowne i sa rozne od spacji to ustalany jest zwyciezca)
 	
 if(tab[3][3]==tab[7][7] && tab[7][7]==tab[11][11] && tab[11][11]!=' '){
 	*wygrany = tab[3][3];
